90d1dce58d036e3de14a9d71f39b505c4b5fc573
Cart Valderama <cartantonio2000@gmail.com>
2024-09-08 09:13:22 +0200

    finished the tutorial

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 export default function Todos() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Todos</h1>
       {todos.map((todo) => (
-        <p key={todo.id}>{todo.text}</p>
+        <div className="flex gap-x-7 justify-center">
+          <p key={todo.id}>{todo.text}</p>
+          <button onClick={() => dispatch(removeTodo(todo.id))}>remove</button>
+        </div>
       ))}
     </div>
   );
